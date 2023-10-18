1ce8445ca4e85d5b0505f26672afe06c87734afe
KA <xla.2@yandex.ru>
2023-10-18 13:43:37 +0300

    2nd task started, cleanup

---
+++ aux_func.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+def rr(env):
+    """quick render fix assuming env's render_mode='rgb_array' """
+    # plt.figure(figsize=(5,10))
+    plt.axis('off')
+    plt.imshow(env.render())
+
+
+def act(action_id: int, env):
+    """filters unnecessary output"""
+    return env.step(action_id)[:3]
